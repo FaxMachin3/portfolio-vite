665708230d70f6c6d822ef306cb9589e19cf9889
subhamraj <subhamraj4114@gmail.com>
2023-04-07 14:48:34 +0530

    vanilla_fullpage

--- src/component/app/App.jsx
+++ src/component/app/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import ReactFullpage from '@fullpage/react-fullpage';
+import React, { useState, useEffect } from 'react';
 import {
     SECTIONS,
     SLIDE_DELAY,
@@ -14,6 +13,7 @@ import useAnimate from '../../hooks/useAnimate';
 import useTheme from '../../hooks/useTheme';
 import useArrowNavigation from '../../hooks/useArrowNavigation';
 import useLoader from '../../hooks/useLoader';
+import fullpage from 'fullpage.js';
 
 import './App.scss';
 
@@ -29,6 +29,20 @@ const App = () => {
     useAnimate(destination, sectionRefs);
     useLoader();
 
+    useEffect(() => {
+        new fullpage('#fullpage', {
+            autoScrolling: true,
+            scrollHorizontally: true,
+            licenseKey: 'YOUR_KEY_HERE',
+            scrollingSpeed: SLIDE_DELAY,
+            anchors: ANCHORS,
+            onLeave: (_origin, destination, _direction) =>
+                setDestination(destination.anchor),
+            easingcss3: 'cubic-bezier(0.785, 0.135, 0.150, 0.860)',
+            dragAndMove: true,
+        });
+    }, []);
+
     return (
         <Error>
             <Navbar
@@ -41,29 +55,16 @@ const App = () => {
                 destination={destination}
                 setDestination={setDestination}
             />
-            <ReactFullpage
-                licenseKey={'YOUR_KEY_HERE'}
-                scrollingSpeed={SLIDE_DELAY}
-                anchors={ANCHORS}
-                onLeave={(_origin, destination, _direction) =>
-                    setDestination(destination.anchor)
-                }
-                easingcss3="cubic-bezier(0.785, 0.135, 0.150, 0.860)"
-                dragAndMove
-                render={(rfpProps) => (
-                    <ReactFullpage.Wrapper>
-                        {SECTIONS.map(({ Component, anchor }) => (
-                            <section key={anchor} className="section">
-                                <Component
-                                    destination={destination}
-                                    setSectionRefs={setSectionRefs}
-                                    {...rfpProps}
-                                />
-                            </section>
-                        ))}
-                    </ReactFullpage.Wrapper>
-                )}
-            />
+            <div id="fullpage">
+                {SECTIONS.map(({ Component, anchor }) => (
+                    <section key={anchor} className="section">
+                        <Component
+                            destination={destination}
+                            setSectionRefs={setSectionRefs}
+                        />
+                    </section>
+                ))}
+            </div>
         </Error>
     );
 };
